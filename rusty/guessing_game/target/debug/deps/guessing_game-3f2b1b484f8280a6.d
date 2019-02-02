d414583eb1796df4bac5bbc10b7ab8dbd7a11272
yngtodd <young.todd.mk@gmail.com>
2019-02-02 17:16:43 -0500

    Starting guessing game

---
+++ rusty/guessing_game/target/debug/deps/guessing_game-3f2b1b484f8280a6.d
@@ -0,0 +1,5 @@
+/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-3f2b1b484f8280a6: src/main.rs
+
+/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-3f2b1b484f8280a6.d: src/main.rs
+
+src/main.rs:
---
+++ rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.d
@@ -0,0 +1,5 @@
+/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.rmeta: src/main.rs
+
+/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.d: src/main.rs
+
+src/main.rs:
