13e288c301fa01b1b83acd67bfc4881eaae2fef2
yngtodd <young.todd.mk@gmail.com>
2019-02-02 17:18:48 -0500

    cleaning up with gitignore

--- rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.d
+++
@@ -1,5 +0,0 @@
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.rmeta: src/main.rs
-
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-f5e1ca0979fb1fcb.d: src/main.rs
-
-src/main.rs:
