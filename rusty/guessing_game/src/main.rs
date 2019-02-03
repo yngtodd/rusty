57414e7a46dfa87d14757aee42010c18d93a3cb4
yngtodd <young.todd.mk@gmail.com>
2019-02-02 21:56:37 -0500

    finish game

--- rusty/guessing_game/src/main.rs
+++ rusty/guessing_game/src/main.rs
@@ -2,36 +2,32 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
-fn main() 
-{
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(0,101);
+
     println!("Guess what number I am thinking of!");
     println!("Please input your guess.");
 
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(0,101);
+    loop {
+        let mut guess = String::new();
 
-    println!("The secret number is {}", secret_number);
-
-    loop 
-    {
         io::stdin().read_line(&mut guess)
             .expect("failed to read line.");
 
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed {}", guess);
 
-        match guess.cmp(&secret_number)
-        {
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater =>  println!("Too big!"),
-            Ordering::Equal => 
-            {
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
-
     }
 }
