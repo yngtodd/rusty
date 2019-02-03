041c4ba64899b9549a7b8b7a359e8b4a4bb8e145
yngtodd <young.todd.mk@gmail.com>
2019-02-02 21:58:07 -0500

    removing unnecessary

--- rusty/guessing_game/target/debug/deps/guessing_game-4dd6befaf892af67.d
+++
@@ -1,5 +0,0 @@
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-4dd6befaf892af67.rmeta: src/main.rs
-
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/guessing_game-4dd6befaf892af67.d: src/main.rs
-
-src/main.rs:
--- rusty/guessing_game/target/debug/deps/rand-407648b594a758e9.d
+++
@@ -1,7 +0,0 @@
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/rand-407648b594a758e9.rmeta: /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/yngtodd/src/develop/rusty/rusty/guessing_game/target/debug/deps/rand-407648b594a758e9.d: /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
-
-/Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
-/Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
-/Users/yngtodd/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
