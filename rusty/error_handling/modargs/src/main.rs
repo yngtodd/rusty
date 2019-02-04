24e587c8dc6762aee62328380550311b29f93e45
yngtodd <young.todd.mk@gmail.com>
2019-02-04 16:17:44 -0500

    simplifying with macro for parser

--- rusty/error_handling/modargs/src/main.rs
+++ rusty/error_handling/modargs/src/main.rs
@@ -6,24 +6,12 @@ use clap::App;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let args = App::from_yaml(yaml).get_matches();
-    let num_str = args.value_of("num");
+    let num = value_t!(args.value_of("num"), i32).unwrap_or_else(|e| e.exit());
 
-    match num_str {
-        None => println!("Need an integer to divide by."),
-        Some(s) => {
-            match s.parse::<i32>() {
-                Ok(n) => {
-                    match divide_ten_by(n) {
-                        Ok(result) => println!("Result = {}", result),
-                        Err(e) => println!("ERROR: {}", e)
-                    };
-                },
-                Err(_) => {
-                    println!("That's not an integer! {}", s)
-                },
-            }
-        }
-    }
+    match divide_ten_by(num) {
+        Ok(result) => println!("Result = {}", result),
+        Err(e) => println!("ERROR: {}", e)
+    };
 }
 
 fn divide_ten_by(i: i32) -> Result<i32, String> {
